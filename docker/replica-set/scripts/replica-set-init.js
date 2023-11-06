c065c28adc94bfe55279dce6b80316193486b240
Vadym Mulish <43036478+WalrusPUNCH@users.noreply.github.com>
2023-11-06 21:21:06 +0200

    Add docker-compose and accompanying files for running mongo DB replica set and multiple standalone instances (#1)
    * add docker-compose and accompanying files for running mongo DB replica set
    
    * add docker-compose and accompanying files for running multiple mongoDB instances

---
+++ docker/replica-set/scripts/replica-set-init.js
@@ -0,0 +1,29 @@
+﻿var config = {
+    "_id": "banking_set",
+    "version": 1,
+    "writeConcernMajorityJournalDefault": false,
+    "members": [
+        {
+            "_id": 0,
+            "host": "mongodb-one:27027",
+            "priority": 1
+        },
+        {
+            "_id": 1,
+            "host": "mongodb-two:27028",
+            "priority": 0.5
+        },
+        {
+            "_id": 2,
+            "host": "mongodb-three:27029",
+            "priority": 0.5
+        }
+    ]
+};
+
+printjson(rs.initiate(config, { force: true }));
+
+console.log('Waiting for replica set initiation...')
+sleep(15000);
+
+printjson(rs.status());
